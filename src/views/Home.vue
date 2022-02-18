0aca8247f47f76698ae98f0c104d2062115d09ac
Rick Walters <rick@walters.xyz>
2022-02-18 12:16:17 -0500

    fixed README and Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="view home">
     <h1>“D3 for the Impatient” Exercises</h1>
+    <p>An exercise in learning D3.js in depth, while simultaneously integrating it with
+      Vue.js 3.0 <strong><em>and</em></strong> exploring some real-world(-ish) use cases. Based on
+      Philipp K. Janert's <a
+        href="https://learning.oreilly.com/library/view/d3-for-the/9781492046783/" target="_blank">D3
+      for the ImpatientL</a>.</p>
+      <p>See the README at the
+      <a href="https://github.com/jpgringo/d3_impatient" target="_blank">GitHub repository</a>
+      for full details.</p>
+
+    <aside>(Chapter 01 isn't missing… there just weren't any code-based exercises there!</aside>
   </div>
 </template>
 
